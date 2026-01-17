ae90eb017d3d55e3d619c5093571932cb5bb493a
oaitbenh-21 <0xmrrandom@gmail.com>
2026-01-18 00:14:24 +0100

    change header links to navigate

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,14 +1,34 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // Import Router for navigation
 import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
-  avatar = "https://github.com/mdo.png"
-  username = jwtDecode.toString()
-  role = "admin"
+  avatar = "https://github.com/mdo.png";
+  username = jwtDecode.toString();
+  role = "admin";
+
+  constructor(private router: Router) { }
+
+  navigateToHome() {
+    this.router.navigate(['/']);
+  }
+
+  navigateToProfile() {
+    this.router.navigate(['/profile', 1]);
+  }
+
+  navigateToAdminDashboard() {
+    this.router.navigate(['/admin/dashboard']);
+  }
+
+  logout() {
+    this.router.navigate(['/logout']);
+  }
 }
