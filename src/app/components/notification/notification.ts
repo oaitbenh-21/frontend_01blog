c57bf661777e5dece87ccfe0021a4ad037ef1c0b
oaitbenh-21 <0xmrrandom@gmail.com>
2026-01-28 18:38:28 +0100

    notification logic added

--- src/app/components/notification/notification.ts
+++ src/app/components/notification/notification.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NotificationService } from '../../services/notification.service';
+import { TimeAgoPipe } from '../../../pipes/timeAgo';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-notification',
-  imports: [],
+  imports: [TimeAgoPipe, NgIf, NgFor],
   templateUrl: './notification.html',
   styleUrl: './notification.scss',
 })
-export class Notification {
-  unreadedCount = 1;
+export class Notification implements OnInit {
+  unreadedCount = 0;
+  constructor(private notifyService: NotificationService) { }
+  notifications: NotificationDto[] = [];
+  ngOnInit(): void {
+    this.notifyService.getNotifications().subscribe({
+      next: (value) => {
+        this.notifications = value;
+        this.unreadedCount = this.getUnreadedCount();
+      },
+      error(err) {
+        console.log(err);
+      },
+    })
+  }
+
+  getUnreadedCount(): number {
+    return this.notifications.filter(n => !n.read).length;
+  }
+
+  markAsRead(notification: NotificationDto) {
+    notification.read = true;
+    // TODO: call API to update backend
+  }
+
+  markAllAsRead() {
+    this.notifications.forEach(n => n.read = true);
+    // TODO: call API
+  }
+
+  deleteNotification(id: number) {
+    this.notifications = this.notifications.filter(n => n.id !== id);
+    // TODO: call API
+  }
 
 }
